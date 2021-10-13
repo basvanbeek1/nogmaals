a5f035677a504d03df7b96214bf63bb4c47ab945
basvanbeek1 <99069932@mydavinci.nl>
2021-10-13 11:54:29 +0200

    alle opdrachten versturen

---
+++ duizelig/optellen.py
@@ -0,0 +1,5 @@
+i = 50
+while i <= 1000:
+    print(i)
+    i = i + 1
+    
---
+++ duizelig/quit.py
@@ -0,0 +1,7 @@
+while True:
+    welke_naam = input("wat is je naam") 
+    if welke_naam == "bas":
+        break
+    else:
+        print("je hebt het niet goed")
+
